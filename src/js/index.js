87a2e912f47c772a5feb49619bdca72b257bf513
Bauyrzhan <baur3dn@gmail.com>
2020-04-26 16:02:22 +0600

    added slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,11 @@
-console.info('init app')
+import Swiper from "swiper";
+
+const swiper = new Swiper(".swiper-container", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-next",
+    prevEl: ".swiper-prev",
+  },
+});
