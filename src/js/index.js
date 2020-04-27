94a10f6cc52f6457508bb6e97bf7c7a5c8918aa4
Bauyrzhan <baur3dn@gmail.com>
2020-04-27 12:05:50 +0600

    added tabs

--- src/js/index.js
+++ src/js/index.js
@@ -9,3 +9,23 @@ const swiper = new Swiper(".swiper-container", {
     prevEl: ".swiper-prev",
   },
 });
+
+const openCity = (evt) => {
+  let i, tabcontent, tablinks;
+  const { name } = evt.currentTarget.dataset;
+  tabcontent = document.getElementsByClassName("material__tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(name).style.display = "block";
+  evt.currentTarget.className += " active";
+};
+
+const tablinks = document.querySelectorAll(".tablink");
+tablinks.forEach(function (tablink) {
+  tablink.onclick = openCity
+});
